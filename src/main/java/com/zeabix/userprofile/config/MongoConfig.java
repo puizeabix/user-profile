42aed0ab8cf60f7364917b2cb09eefb4a97e6e17
Prawit Chaivong <prawit.c@aware.co.th>
2022-04-22 16:24:52 +0700

    Add mongodb connection and configuration

---
+++ src/main/java/com/zeabix/userprofile/config/MongoConfig.java
@@ -0,0 +1,31 @@
+package com.zeabix.userprofile.config;
+
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${persistent.mongodb.connection}")
+    private String connectionUrl;
+
+    @Value(("${persistent.mongodb.dbname}"))
+    private String dbName;
+
+    @Bean
+    public ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory(){
+        return new SimpleReactiveMongoDatabaseFactory(
+                MongoClients.create(connectionUrl), dbName
+        );
+    }
+
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate(){
+        return new ReactiveMongoTemplate(reactiveMongoDatabaseFactory());
+    }
+}
